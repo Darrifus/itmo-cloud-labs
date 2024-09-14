3023ad41f91993f158d570e03c6b2b1adef54644
Alexander <115710618+Landonimous@users.noreply.github.com>
2024-09-14 17:17:47 +0300

    Delete DevOps Лаба 2/MyScript.py

--- DevOps Лаба 2/MyScript.py
+++
@@ -1,7 +0,0 @@
-a = [8, 7, 6, 5, 4, 3, 2, 1]
-for i in range(len(a)):
-    for j in range(len(a) - 1):
-        if a[j] > a[j + 1]:
-            x = a[j + 1]
-            a[j + 1] = a [j]
-            a[j] = x
